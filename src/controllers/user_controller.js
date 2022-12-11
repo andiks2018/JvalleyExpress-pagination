11bab7de1ca31d8479fce3687646c739f2e5c068
andi <andiks2018@gmail.com>
2022-12-11 22:10:59 +0700

    upload

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -23,9 +23,29 @@ export const user_create = async (req = request, res = response) => {
 // user read
 export const user_read = async (req = request, res = response) => {
     try {
-        const result = await db.users.findMany()
+
+        // let page =1
+        // let limit = 10
+        let {page=1, limit=10}= req.query //menghasilkan string
+        let skip = (page-1) * limit
+        const result = await db.users.findMany({
+            //take : 10,
+            take : parseInt(limit),
+            //skip:10
+            skip:skip
+        })
+
+        //informasi total data keseluruhan 
+        const resultCount = await db.users.count()//integer jumlah total data user
+
+        //generated total page
+        const totalPage= Math.ceil(resultCount/limit)
+
         return res.status(200).json({
             success: true,
+            current_page: page - 0, //ini -0 merubah menjadi integer
+            total_page : totalPage,
+            total_data : resultCount,
             data: result
         })
     } catch (error) {
